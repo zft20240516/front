815ecd13d19e6939561bb50a75f6436fcb17a977
zft20240516 <zft20240516@gmail.com>
2024-05-23 18:51:57 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function doOnClick(e){
   alert("unknown click")
   fetch(
       "https://script.google.com/macros/s/AKfycbxQIauCFmNGu_PPzqCrwxPVoDTQw4L5qoJmnGHje8ujTDFUj36EqaO1EKixgp8vsdk6/exec/api/ping/",
-      {redirect: "follow",}
+      {redirect: "follow", mode: "no-cors"}
   ).then((resp) => {
     resp.text().then((s) => {
       document.getElementById("status").innerText = s
