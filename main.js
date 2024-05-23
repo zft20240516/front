ac721651d4a87dc5acef8e19b9227e7cddb7a032
zft20240516 <zft20240516@gmail.com>
2024-05-23 11:22:56 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function doOnClick(e){
   alert("unknown click")
-  fetch("https://script.google.com/macros/s/AKfycbxQIauCFmNGu_PPzqCrwxPVoDTQw4L5qoJmnGHje8ujTDFUj36EqaO1EKixgp8vsdk6/exec/api/ping/").then(resp) => {
-    resp.text().then(s) => {
+  fetch("https://script.google.com/macros/s/AKfycbxQIauCFmNGu_PPzqCrwxPVoDTQw4L5qoJmnGHje8ujTDFUj36EqaO1EKixgp8vsdk6/exec/api/ping/").then((resp) => {
+    resp.text().then((s) => {
       document.getElementById("status").innerText = s
-    }
-  }
+    })
+  })
 }
