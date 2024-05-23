384603cf936ed88f8415995bd71db1fec6df7f8b
zft20240516 <zft20240516@gmail.com>
2024-05-23 11:44:59 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function doOnClick(e){
   alert("unknown click")
-  fetch("https://script.google.com/macros/s/AKfycbxQIauCFmNGu_PPzqCrwxPVoDTQw4L5qoJmnGHje8ujTDFUj36EqaO1EKixgp8vsdk6/exec/api/ping/").then((resp) => {
+  fetch(
+      "https://script.google.com/macros/s/AKfycbxQIauCFmNGu_PPzqCrwxPVoDTQw4L5qoJmnGHje8ujTDFUj36EqaO1EKixgp8vsdk6/exec/api/ping/",
+      {redirect: "follow",}
+  ).then((resp) => {
     resp.text().then((s) => {
       document.getElementById("status").innerText = s
     })
