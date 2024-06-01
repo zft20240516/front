0737b91d755c0e879bdd31b886f7c122ec800ad6
zft20240516 <zft20240516@gmail.com>
2024-06-01 21:37:41 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function doOnClick(e){
   alert("unknown click")
   fetch(
       "https://script.google.com/macros/s/AKfycbxQIauCFmNGu_PPzqCrwxPVoDTQw4L5qoJmnGHje8ujTDFUj36EqaO1EKixgp8vsdk6/exec",
-      {redirect: "manual", amode: "no-cors"}
+      {method: "POST", redirect: "follow", amode: "no-cors", headers: {"Content-Type": "text/plain"}, body: '{"aaa": "bbb", "bbb": "aaa", "ccc": ["bbb", "aaa", "ccc"], "action": "pinga"}'}
   ).then((resp) => {
     console.log(resp);
     resp.text().then((s) => {
