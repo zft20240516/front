7576c59e09da37e0894f2db43bdb6ceb7d3f4cc6
zft20240516 <zft20240516@gmail.com>
2024-06-02 00:48:30 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
+function dgebi(id){return document.getElementById(id)}
+
 function doOnClick(e){
-  alert("unknown click")
+  let query = dgebi(query).value.split("\r");
+  let action = query.shift().trim();
+  let parameter = {}
+  for (const s of query){
+    let a = s.split(":",2);
+    let name = a.shift().trim(); if (name == ""){continue}
+    if (a.length = 0){a[0]=""}
+    parameter[name] = a[0];
+  }
   fetch(
       "https://script.google.com/macros/s/AKfycbxQIauCFmNGu_PPzqCrwxPVoDTQw4L5qoJmnGHje8ujTDFUj36EqaO1EKixgp8vsdk6/exec",
-      {method: "POST", redirect: "follow", amode: "no-cors", headers: {"Content-Type": "text/plain"}, body: '{"aaa": "bbb", "bbb": "aaa", "ccc": ["bbb", "aaa", "ccc"], "action": "pinga"}'}
+      {
+        method: "POST",
+        redirect: "follow",
+        amode: "no-cors",
+        headers: {"Content-Type": "text/plain"},
+        body: `{"aaa": "bbb", "bbb": "aaa", "ccc": ["bbb", "aaa", "ccc"], "action": ${action}, "parameter": ${JSON.stringify(parameter)}}`
+      }
   ).then((resp) => {
     console.log(resp);
     resp.text().then((s) => {
       console.log(s);
-      document.getElementById("status").innerText = s;
+      dgebi("status").innerText = s;
     })
   })
 }
