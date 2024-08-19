57d09530298f91362938233442ae03f880f7dd06
zft20240516 <zft20240516@gmail.com>
2024-08-19 13:23:18 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-function test1(){
-  
+function eClick_btnTest(e){
+  console.log("Click handler for <btnTest> fired.")
 }
 
 window.addEventListener("click", (e) => {
+  if ((const _EventName = "eClick_" + e.target.id) && (const _Handler = window[_EventName]) && (typeOf(_Handler == "function"))){_Handler(e); return}
   console.log(e);
 })
